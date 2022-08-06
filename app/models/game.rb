304aaef376c0e96febbbd2a3e0a6aacfdcf54900
Bobby Bates <robertfbatesjr@gmail.com>
2022-08-06 19:43:02 -0400

    complete assignment

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,9 @@
 class Game < ActiveRecord::Base
-  
+  # DIY method:
+  # def reviews
+  #   Review.where(game_id: self.id)
+  # end
+
+  # AR macro:
+  has_many :reviews
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ActiveRecord::Base
-  
+  # A review belongs to a game
+  # def game
+  #   # self is the review instance
+  #   Game.find(self.game_id)
+  # end
+
+  # Updated with AR macro:
+  belongs_to :game
 end
